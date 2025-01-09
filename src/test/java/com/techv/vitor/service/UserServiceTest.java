1533bfd734cb8634b92ea93e7bc458d2e6f3ce0a
Vitor <vitorbonatocam@gmail.com>
2025-01-08 23:48:06 -0300

    fix. fix verify login test

--- src/test/java/com/techv/vitor/service/UserServiceTest.java
+++ src/test/java/com/techv/vitor/service/UserServiceTest.java
@@ -103,7 +103,6 @@ class UserServiceTest {
         User mock = new User(1L, "Vitor", "teste@email.com", "12345", Integrated.TRUE, LocalDateTime.now());
         Mockito.when(userRepository.findByUsername(mock.getUsername())).thenReturn(Optional.of(mock));
         var request = new LoginRequest(mock.getUsername(), mock.getPassword());
-        Mockito.when(userService.verifyLogin(request)).thenReturn(Boolean.TRUE);
         var response = userService.verifyLogin(request);
 
         Assertions.assertTrue(response);
