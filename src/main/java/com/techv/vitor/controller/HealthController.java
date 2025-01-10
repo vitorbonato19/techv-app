05b7f03d8ecbed5b6982c831e5996fb3a2926b0d
Vitor <vitorbonatocam@gmail.com>
2025-01-09 23:41:24 -0300

    raw. LogSL4J dependencies, Main manifest error resolved

--- src/main/java/com/techv/vitor/controller/HealthController.java
+++ src/main/java/com/techv/vitor/controller/HealthController.java
@@ -2,6 +2,8 @@ package com.techv.vitor.controller;
 
 import com.techv.vitor.service.UserService;
 import org.apache.coyote.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/health")
 public class HealthController {
+
+    private static final Logger log = LoggerFactory.getLogger(HealthController.class);
 
     private UserService userService;
 
@@ -20,6 +24,7 @@ public class HealthController {
     @GetMapping
     public ResponseEntity<String> healthCheckApplication() {
         var ok = userService.findAllHealthCheck();
+        log.info("Healthcheck endpoint ok!");
         return ResponseEntity.status(200).body("Application ok!");
     }
 }
