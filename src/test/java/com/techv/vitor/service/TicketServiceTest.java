d8d02b80ddc8d452d0f9787b7973683227bad111
Vitor <vitorbonatocam@gmail.com>
2025-02-10 21:57:18 -0300

    test. ticket service findById()

--- src/test/java/com/techv/vitor/service/TicketServiceTest.java
+++ src/test/java/com/techv/vitor/service/TicketServiceTest.java
@@ -1,6 +1,7 @@
 package com.techv.vitor.service;
 
 import com.techv.vitor.entity.Ticket;
+import com.techv.vitor.exception.TicketNotFoundException;
 import com.techv.vitor.mapper.TicketMapper;
 import com.techv.vitor.repository.TicketRepository;
 import org.junit.jupiter.api.Assertions;
@@ -16,6 +17,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 class TicketServiceTest {
@@ -48,8 +50,11 @@ class TicketServiceTest {
 
             var response = ticketService.findAll();
 
-            Assertions.assertNotNull(response);
-            Assertions.assertEquals(response, tickets);
+
+            Assertions.assertAll(
+                    () -> Assertions.assertNotNull(response),
+                    () -> Assertions.assertEquals(response, tickets)
+            );
         }
 
         @Test
@@ -60,7 +65,11 @@ class TicketServiceTest {
 
             var response = ticketService.findAll();
 
-            Assertions.assertEquals(response, tickets);
+            Assertions.assertAll(
+                    () -> Assertions.assertEquals(response, tickets),
+                    () -> Assertions.assertTrue(response.isEmpty())
+            );
+
         }
 
         @Nested
@@ -70,7 +79,41 @@ class TicketServiceTest {
             @DisplayName("should return the ticket entity by the id on path params")
             void shouldReturnTicketEntityIfTheIdExists() {
 
-                var mock 
+                var mock = new Ticket.TicketBuilder()
+                        .id(1L)
+                        .analyst("vitor")
+                        .requester("teste")
+                        .text("teste2text")
+                        .reply("null")
+                        .build();
+
+                Mockito.when(ticketRepository.findById(mock.getId())).thenReturn(Optional.of(mock));
+
+                var response = ticketService.findById(mock.getId());
+
+                Assertions.assertAll(
+                        () -> Assertions.assertNotNull(response),
+                        () -> Assertions.assertEquals(response.getId(), mock.getId()),
+                        () -> Assertions.assertEquals(response.getAnalyst(), mock.getAnalyst()),
+                        () -> Assertions.assertEquals(response.getRequester(), mock.getRequester()),
+                        () -> Assertions.assertEquals(response.getText(), mock.getText()),
+                        () -> Assertions.assertEquals(response.getReply(), mock.getReply())
+                );
+            }
+
+            @Test
+            @DisplayName("Should throw a exception if the id not exists on DB")
+            void shouldThrowExceptionIfIdNotExists() {
+
+                var mock = new Ticket.TicketBuilder()
+                        .id(1L)
+                        .analyst("vitor")
+                        .requester("teste")
+                        .text("teste2text")
+                        .reply("null")
+                        .build();
+
+                Assertions.assertThrows(TicketNotFoundException.class, () -> ticketService.findById(mock.getId()));
             }
         }
 
